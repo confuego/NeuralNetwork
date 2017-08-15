98a4f57b3bb4dbf6e33d711e249e54336f569fdd
Wesley <wes.chappell10@gmail.com>
2017-08-15 13:36:59 -0400

    add solution

---
+++ NeuralNetwork/NeuralNetwork/Matrix.cs
@@ -0,0 +1,15 @@
+﻿namespace NeuralNetwork
+{
+    public class Matrix
+    {
+        public int Rows { get; set; }
+        
+        public int Columns { get; set; }
+        
+        public Matrix(int rows, int columns)
+        {
+            Rows = rows;
+            Columns = columns;
+        }
+    }
+}
---
+++ NeuralNetwork/NeuralNetworkConsoleApplication/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using NeuralNetwork;
+
+namespace NeuralNetworkConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var matrix = new Matrix(1, 1);
+            Console.WriteLine(matrix.Columns);
+        }
+    }
+}
